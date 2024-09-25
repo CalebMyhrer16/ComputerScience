de0d7737e93987aec1d5a3c05356744540b74bde
CalebMyhrer16 <820283@mystma.org>
2024-09-25 14:59:26 -0500

    commenting on animal survey

--- Unit 2 - Functions and Variables/Practice_Sept-24.py
+++
@@ -1,4 +0,0 @@
-user_input1 = input("what is your favorite animal: ")
-user_input2 = input("what color is the chipmink: ")
-user_input3 = input("where is the chipmunk found: ")
-user_input4 = input("why do you like that animal: ")
--- Unit 2 - Functions and Variables/animal_survey.py
+++ Unit 2 - Functions and Variables/animal_survey.py
@@ -1,5 +1,5 @@
-user_input1 = input("what is your favorite animal: ")
-user_input2 = input("what color is the chipmink: ")
-user_input3 = input("where is the chipmunk found: ")
-user_input4 = input("why do you like that animal: ")
-print("what is your favorite animal " +  user_input1  + " what color is the chipmunk " + user_input2 + " where is the chipmunk found " + user_input3 + " why do you like the animal ")
+user_input1 = input("what is your favorite animal: ") #asks questiona nd lets you answer
+user_input2 = input("what color is the animal: ") #asks questiona nd lets you answer
+user_input3 = input("where is the animal found: ") #asks questiona nd lets you answer
+user_input4 = input("why do you like that animal: ") #asks questiona nd lets you answer
+print("what is your favorite animal " +  user_input1  + " what color is the animal " + user_input2 + " where is the animal found " + user_input3 + " why do you like the animal ") #shows you all your answer in a list at the end
