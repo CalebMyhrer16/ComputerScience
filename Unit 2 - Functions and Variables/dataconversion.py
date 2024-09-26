dffa3d3fc93a3471b46a25870c0ba426ecc8b5ad
CalebMyhrer16 <820283@mystma.org>
2024-09-26 14:24:27 -0500

    data conversion

--- Unit 2 - Functions and Variables/dataconversion.py
+++ Unit 2 - Functions and Variables/dataconversion.py
@@ -6,12 +6,12 @@
 a = str(115)         #int -> string
 b = int(3.14)      #float -> string
 c = int("68" )      #string -> int
-d = bool "True"      #string -> boolean
+d = bool (True)      #string -> boolean
 e = str (True)     #boolean -> string
 f = str (False)      #boolean -> string
 g = '10110111'  #byte -> int
 h = "2.54"      #string -> float
-i = 100         #int -> float
-j = 10.0        #float -> int
-k = 254         #int -> byte
+i = float(100)         #int -> float
+j = int(10.0)       #float -> int
+k = bytes (254)         #int -> byte
 
