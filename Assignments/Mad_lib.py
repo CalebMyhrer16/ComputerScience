28ae129ce84106a69f4bc420db63973b55acbd7e
CalebMyhrer16 <820283@mystma.org>
2024-09-27 14:18:37 -0500

    mad lib

---
+++ Assignments/Mad_lib.py
@@ -0,0 +1,12 @@
+adjective_1 = input("give one adjective ") #lets user choose an adjective
+noun_1 = input("give one noun place ") #lets user choose a noun
+adjective_2 = input("give diffrenet adjective ") #lets user choose an adjective
+noun_3 = input(" type a person ") #user gets to pick a noun
+verb_1 = input("give a verb ") #user gets to pick a verb
+adverb_1 = input("give a adverb ") #user picks a verb
+adjective_3 = input(" new adjective ") # lets user choose a adjective
+noun_2 = input(" an animal ") # lets user choose a new noun
+verb_2 = input("new verb ") # lets user choose a new verb
+adjective_4 = input("final adjective ") #lets user choose a final adjective
+verb_3 = input("name a verb ") #lets user pick a final verb
+print("one day a " + adjective_1 + " " + noun_1 + " lived in a big" + adjective_2 + " " + noun_1 + " ounce in a while the " + noun_3  + " would " + verb_1 + " " + adverb_1 + " a year later " + adjective_3 + " " + noun_2 + " jumped " + "out of a hole and " + verb_2 + " the " + noun_1 + " pushed him back " + " The hole dissapered" + noun_1 + " was " + adjective_4 + " and was really " + verb_3) #shows full gooofy ahh mad lib
